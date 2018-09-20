f10e61669227967cf1c1862a9a16f1ed312c8f04
Jason Walsh <jwalsh@saylent.com>
2018-09-20 09:44:27 -0400

    jwt.el

--- jwt.el
+++ jwt.el
@@ -1,43 +1,83 @@
-;;; jwt.el --- JWT Utilities
+;;; jwt.el --- JWT token utilities                   -*- lexical-binding: nil; -*-
 
-;; Copyright (C) 2018 Free Software Foundation, Inc.
+;; Copyright (C) 2018  Jason Walsh
 
 ;; Author: Jason Walsh <j@wal.sh>
+;; Keywords: tools
+
+;; Filename: jwt.el
+;; Description: Encode, decode, and parse JWT tokens.
+;; Maintainer:
+;; Created: Wed Sep 19 13:52:38 2018 (-0400)
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24.1") (cl-lib "0.5") (json "1.4") (dash "2.10.0") (s "1.11.0")  (ht "2.2"))
-;; Keywords: jwt, security
-;; URL: http://wal.sh/jwt
-
+;; Package-Requires: ()
+;; URL: http://wal.sh/research/jwt
+;; Doc URL: http://wal.sh/research/jwt
+;; Keywords: tools
+;; Package-Requires: ((cl-lib "0.5") (emacs "25"))
+;; Dependencies: ((cl-lib "0.5") (emacs "25"))
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Commentary:
-
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Code:
 
-(require 'request)
+(provide 'jwt)
 
 (defgroup jwt nil
   "Tools for working with JWT tokens."
   :group 'tools)
 
-(setq jwt-algorithms '((:ecdsa "ECDSA")
-                       (:hmac "HMAC")
-                       (:none "None")
-                       (:rsa "RSA")))
+(defconst jwt-algorithms-alist
+  (list '((:ecdsa "ECDSA")
+          (:hmac "HMAC")
+          (:none "None")
+          (:rsa "RSA"))))
 
-(ert-deftest test-jwt-algorithm-defaut ()
-          (should (= (alist-get :hmac jwt-algorithms) "HMAC")))
+(ert-deftest jwt-test-algorithm-defaut ()
+  (should (= (alist-get :hmac jwt-algorithms-alist) "HMAC")))
 
-(defun jwt-encode (payload secret algorithm))
+(defun jwt-encode (payload secret algorithm)
+  "Create a JWT Token for a PAYLOAD with a shared SECRET using ALGORITHM (defaults to HMAC)."
+  (message "jwt-encode"))
 
-(defun jwt-decode (token))
+(defun jwt-decode (token)
+  "Decode the payload for a JWT TOKEN."
+  (message "jwt-decode"))
 
 (defvar jwt-run-tests nil)
 
 (eval-when-compile
   (setq jwt-run-tests t))
 
-
 (when jwt-run-tests
    (ert-deftest addition-test ()
        (should (= (+ 1 2) 4))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; jwt.el ends here
